9472513f55312ee468a56c4a682340287dad636a
“JuanDiaz” <juanluisdiazguerrero@gmail.com>
2023-03-20 23:26:48 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Prueba from './components/Prueba'
+
+function App () {
+  return (
+    <div className="App">
+      <Prueba mensaje="Hola mundo" nombre="Juan" edad="25" />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Prueba.jsx
@@ -0,0 +1,13 @@
+
+const Prueba = ({ mensaje, nombre, edad }) => {
+  return (
+    <div>
+        <h1>{nombre}</h1>
+        <h2>{mensaje}</h2>
+        <h3>{edad}</h3>
+    </div>
+
+  )
+}
+
+export default Prueba
