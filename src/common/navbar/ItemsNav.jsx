d60d8760bb007ce6b554451e8edb1095f9aa56b7
Juan Diaz Guerrero <juanluisdiazguerrero@gmail.com>
2023-04-27 01:19:29 -0500

    add sign out button

--- src/common/navbar/ItemsNav.jsx
+++ src/common/navbar/ItemsNav.jsx
@@ -1,17 +1,33 @@
-import { Box, Stack, useColorModeValue } from '@chakra-ui/react'
-
-import { Link } from 'react-router-dom'
-
+import { Box, Stack, useColorModeValue, Button } from '@chakra-ui/react'
+import { useContext } from 'react'
+import AuthContext from '../../context/auth/AuthContext.jsx'
+import { Link, useNavigate } from 'react-router-dom'
 import { NAV_ITEMS } from '../../data/navitems.js'
 
-import { NAV_ITEMS_LOG } from '../../data/navitemslog.js'
-
-import { useContext } from 'react'
-
-import AuthContext from '../../context/auth/AuthContext.jsx'
-
+// todo: corregir este archivo
 export default function ItemsNav ({ direction, spacing = 10, ...props }) {
-  const { getSession } = useContext(AuthContext)
+  const { getSession, removeSession } = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    console.log('click')
+    removeSession()
+    navigate('/')
+  }
+
+  const isLogged = NAV_ITEMS.map((navItem) => (
+    <Box
+      key={navItem.label}
+      color={useColorModeValue('text.light', 'text.dark')}
+      fontWeight={'semibold'}
+      _hover={{
+        color: useColorModeValue('secondary.light', 'secondary.dark')
+      }}
+    >
+      <Link to={navItem.href ?? '#'}>{navItem.label}</Link>
+    </Box>
+  ))
+
   return (
     <Stack
       direction={direction}
@@ -20,32 +36,32 @@ export default function ItemsNav ({ direction, spacing = 10, ...props }) {
       color={useColorModeValue('primary.light', 'primary.dark')}
       {...props}
     >
-    {!getSession()
-      ? (NAV_ITEMS.map((navItem) => (
-        <Box
-          key={navItem.label}
-          color={useColorModeValue('text.light', 'text.dark')}
-          fontWeight={'semibold'}
-          _hover={{
-            color: useColorModeValue('secondary.light', 'secondary.dark')
-          }}
-        >
-          <Link to={navItem.href ?? '#'}>{navItem.label}</Link>
-        </Box>
-        )))
-      : (NAV_ITEMS_LOG.map((navItemLog) => (
-      <Box
-        key={navItemLog.label}
-        color={useColorModeValue('text.light', 'text.dark')}
-        fontWeight={'semibold'}
-        _hover={{
-          color: useColorModeValue('secondary.light', 'secondary.dark')
-        }}
-      >
-        <Link to={navItemLog.href ?? '#'}>{navItemLog.label}</Link>
-      </Box>
-        )))
-    }
+      {!getSession()
+        ? (
+            isLogged
+          )
+        : (
+        <>
+          <Box
+            color={useColorModeValue('text.light', 'text.dark')}
+            fontWeight={'semibold'}
+            _hover={{
+              color: useColorModeValue('secondary.light', 'secondary.dark')
+            }}
+          >
+            <Link to={'/app/profile'}>Ver Perfil</Link>
+          </Box>
+          <Box
+            color={useColorModeValue('text.light', 'text.dark')}
+            fontWeight={'semibold'}
+            _hover={{
+              color: useColorModeValue('secondary.light', 'secondary.dark')
+            }}
+          >
+            <Button colorScheme='white' variant='outline' onClick={handleClick}>Cerrar Sesion</Button>
+          </Box>
+        </>
+          )}
     </Stack>
   )
 }
