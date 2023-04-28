b1efbf3f3caa6ba0093d44d16a2169dd62702ca6
Juan Diaz Guerrero <juanluisdiazguerrero@gmail.com>
2023-04-28 01:05:22 -0500

    add link to hero button

--- src/LandingPage/components/hero/ButtonHero.jsx
+++ src/LandingPage/components/hero/ButtonHero.jsx
@@ -1,5 +1,6 @@
 import { Stack, useColorModeValue, Button } from '@chakra-ui/react'
 import { FiMap } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
 const ButtonHero = () => {
   return (
@@ -7,18 +8,20 @@ const ButtonHero = () => {
       spacing={{ base: 4, sm: 6 }}
       direction={{ base: 'column', sm: 'row' }}
     >
-      <Button
-        rounded={'full'}
-        size={'lg'}
-        fontWeight={'bold'}
-        px={6}
-        colorScheme={'red'}
-        bg={useColorModeValue('secondary.light', 'secondary.dark')}
-        _hover={{ bg: 'red.500' }}
-        leftIcon={<FiMap />}
-      >
-        Ver Rutas
-      </Button>
+      <Link to='/app/map'>
+        <Button
+          rounded={'full'}
+          size={'lg'}
+          fontWeight={'bold'}
+          px={6}
+          colorScheme={'red'}
+          bg={useColorModeValue('secondary.light', 'secondary.dark')}
+          _hover={{ bg: 'red.500' }}
+          leftIcon={<FiMap />}
+        >
+          Ver Rutas
+        </Button>
+      </Link>
     </Stack>
   )
 }
